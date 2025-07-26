60d8fa87db3aaeed1fb88bcf9146915f0ecdf901
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-07-26 19:02:15 +0530

    Update integer_to_string.c

--- integer_to_string.c
+++ integer_to_string.c
@@ -2,32 +2,31 @@
 
 int main()
 {
-    int num =123;
-    int i=0;
-    char str[4];
-    while(num!=0)
+    int num = 123;
+    int i = 0;
+    char str[4];  
+
+    // Convert number to reverse string
+    while (num != 0)
     {
         int rem = num % 10;
-        
-        if(rem >=0 && rem<=9)
-        {
-            str[i] = rem + '0';
-            i++;
-        }
-        num = num / 10; 
-    }
-    
-    for(int j=0; j<i/2;j++)
-    {
-        char temp = str[j];
-        str[j]= str[i-j-1];
-        str[i-j-1]= temp;
-    }
-    
-    for(int k=0; k<i; k++)
-    {
-        printf("%c",str[k]);
+        str[i] = rem + '0';
+        i++;
+        num = num / 10;
     }
 
-    
+    str[i] = '\0'; // null terminate to make it a valid string
+
+    // Reverse the string to get correct order
+    for (int j = 0; j < i / 2; j++)
+    {
+        char temp = str[j];
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = temp;
+    }
+
+    // Now print the string directly
+    printf("Final string is: %s\n", str);
+
+    return 0;
 }
