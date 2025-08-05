9af510aadd1aacb984403d07d984771ea9fff557
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-05 17:39:07 +0530

    Update search_substring_in_given_string.c

--- search_substring_in_given_string.c
+++ search_substring_in_given_string.c
@@ -5,36 +5,31 @@ int main()
 {
     char s1[] = "hello world";
     char s2[] = "world";
-
+    
     int n = strlen(s1);
     int m = strlen(s2);
-
-    int found = 0;  // assume substring is not found first
-
-    for (int i = 0; i <= n - m; i++)
+    int count = 0;
+    
+    int i=0;
+    int j=0;
+    
+    while(i<n && j<m)
     {
-        int flag = 1;
-
-        for (int j = 0; j < m; j++)
+        if(s1[i]==s2[j])
         {
-            if (s1[i + j] == s2[j])
-                continue;
-            else
-            {
-                flag = 0;
-                break;
-            }
+            j++;
+            count++;
         }
-
-        if (flag)
-        {
-            printf("%s is a substring of main string %s and it's found at %d index\n", s2, s1, i);
-            found = 1;  // match found
-        }
+        
+        i++;
     }
-
-    if (!found)
-        printf("Substring is not found\n");
+    
+   // printf("%d",count);
+    
+    if(count==m)
+    printf("%s is the substring of %s",s2,s1);
+    else
+    printf("Not a substring");
 
     return 0;
 }
