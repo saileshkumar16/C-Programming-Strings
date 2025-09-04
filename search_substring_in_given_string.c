49ced55ca0f9d39314e8b612d66a1b298d299136
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-09-04 21:58:39 +0530

    Update search_substring_in_given_string.c

--- search_substring_in_given_string.c
+++ search_substring_in_given_string.c
@@ -3,20 +3,22 @@
 
 int main()
 {
-    char s1[] = "sailesh";
-    char s2[] = "ile";
+    char s1[]="sailesh";
+    char s2[]="ile";
     
     int n = strlen(s1);
-    int m = strlen(s2);
-    int count = 0;
     
     int i=0;
     int j=0;
+    int count=0;
+    
+    int m = strlen(s2);
     
     while(i<n && j<m)
     {
         if(s1[i]==s2[j])
         {
+            i++;
             j++;
             count++;
         }
@@ -25,14 +27,22 @@ int main()
         {
             j=0;
             count=0;
+            i++;
         }
-        
-        i++;
     }
     
     if(count==m)
-    printf("%s is the substring of %s",s2,s1);
+    printf("%s is a sub string of %s",s2,s1);
     else
-    printf("Not a substring");
+    printf("Not a sub string");
+    
+    
+ 
+ 
+
+    
     return 0;
 }
+
+
+
