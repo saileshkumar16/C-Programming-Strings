77fd9e46241837c9df3a3e902b26e1ad4e1d722d
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-10-08 10:50:23 +0530

    Update reverse_the_words_in_string.c

--- reverse_the_words_in_string.c
+++ reverse_the_words_in_string.c
@@ -3,56 +3,41 @@
 
 int main()
 {
-    char str[]="the pen";
-    
+    char str[] = "the pen is blue";
     int n = strlen(str);
-    
-    int start = 0;
-    int end = n-1;
-    
-    while(start<end)
-    {
-        char tmp = str[start];
-        str[start] = str[end];
-        str[end] = tmp;
-        start++;
-        end--;
-        
-    }
-    
-    int i=0;
-    
-    while(i<n)
-    {
-        int j = i;
-        
-        while(str[j]!='\0' && str[j]!=' ') 
-        j++;
-        
-        
-        int start = i;
-        int end = j-1;
-        
-        while(start<end)
-        {
-        char tmp = str[start];
-        str[start] = str[end];
-        str[end] = tmp;
-        start++;
-        end--;
-            
-        }
-        
-        i = j+1;
-        
-    }
-    
-    
- printf("\nThe new string is\n%s",str);
 
-    
+    // Step 1: Reverse the entire string
+    int start = 0, end = n - 1;
+    while (start < end)
+    {
+        char tmp = str[start];
+        str[start] = str[end];
+        str[end] = tmp;
+        start++;
+        end--;
+    }
+
+    // Step 2: Reverse each word individually using your logic
+    start = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        if (str[i] == ' ' || str[i] == '\0')
+        {
+            int end = i - 1;
+
+            while (start < end)
+            {
+                char tmp = str[start];
+                str[start] = str[end];
+                str[end] = tmp;
+                start++;
+                end--;
+            }
+
+            start = i + 1;
+        }
+    }
+
+    printf("The new string is:\n%s", str);
     return 0;
 }
-
-
-
