9ae481ebe1473d0ea2f809a51a7ee8f4264f8530
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-10-12 15:06:21 +0530

    Update remove_all_occurences.c

--- remove_all_occurences.c
+++ remove_all_occurences.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
 #include <string.h>
-int main() 
-{
-    
-    char str[]="sailesh";
-    
-    int n = strlen(str);
-    
-    int i=0;
-    int j=0;
-    
-    char ch = 's';
-    
-    while(i<n)
-    {
-        if(str[i]==ch)
-        {
-            int j = i;
-            
-            while(j<n)
-            {
-                str[j] = str[j+1];
-                j++;
-            }
-            
-            n--;
-            i--;
-        }
-        
-        i++;
-        
-    }
-    
-    printf("After removing all occurences the string is %s",str);
 
-    
+void remove_char_j0(char str[], char ch) {
+    int n = strlen(str);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {  // Scans from start each time
+            if (str[j] == ch) {
+                // Shift left
+                for (int k = j; k < n - 1; k++)
+                    str[k] = str[k + 1];
+                n--;
+                j--; // stay at same index after shift
+            }
+        }
+    }
+    str[n] = '\0';
+    printf("After removing '%c' (j=0): %s\n", ch, str);
+}
+
+void remove_char_ji(char str[], char ch) {
+    int n = strlen(str);
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {  // Scans only from current i
+            if (str[j] == ch) {
+                // Shift left
+                for (int k = j; k < n - 1; k++)
+                    str[k] = str[k + 1];
+                n--;
+                j--; // stay at same index after shift
+            }
+        }
+    }
+    str[n] = '\0';
+    printf("After removing '%c' (j=i): %s\n", ch, str);
+}
+
+int main() {
+    char str1[] = "saai";
+    char str2[] = "saai";
+    char ch = 'a';
+
+    remove_char_j0(str1, ch);
+    remove_char_ji(str2, ch);
+
     return 0;
 }
