b2eadbb56da1f7dca8ecbc165eae933ddf6159cd
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-10-12 15:42:24 +0530

    Update remove_all_occurences.c

--- remove_all_occurences.c
+++ remove_all_occurences.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-void remove_char_j0(char str[], char ch) {
+void remove_char_j0(char str[], char ch) 
+{
     int n = strlen(str);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {  // Scans from start each time
-            if (str[j] == ch) {
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n; j++)  // Scans from start each time
+        {
+            if (str[j] == ch) 
+            {
                 // Shift left
-                for (int k = j; k < n - 1; k++)
+                for (int k = j; k < n - 1; k++) 
+                {
                     str[k] = str[k + 1];
+                }
                 n--;
                 j--; // stay at same index after shift
             }
@@ -18,14 +24,20 @@ void remove_char_j0(char str[], char ch) {
     printf("After removing '%c' (j=0): %s\n", ch, str);
 }
 
-void remove_char_ji(char str[], char ch) {
+void remove_char_ji(char str[], char ch) 
+{
     int n = strlen(str);
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {  // Scans only from current i
-            if (str[j] == ch) {
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = i; j < n; j++)  // Scans only from current i
+        {
+            if (str[j] == ch) 
+            {
                 // Shift left
-                for (int k = j; k < n - 1; k++)
+                for (int k = j; k < n - 1; k++) 
+                {
                     str[k] = str[k + 1];
+                }
                 n--;
                 j--; // stay at same index after shift
             }
@@ -35,7 +47,8 @@ void remove_char_ji(char str[], char ch) {
     printf("After removing '%c' (j=i): %s\n", ch, str);
 }
 
-int main() {
+int main() 
+{
     char str1[] = "saai";
     char str2[] = "saai";
     char ch = 'a';
