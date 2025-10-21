1ae2ad8693e48896da3c9041548d9026f2a82fbc
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-10-21 19:04:30 +0530

    Update reverse_the_words_in_string.c

--- reverse_the_words_in_string.c
+++ reverse_the_words_in_string.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int main() 
 {
     char str[] = "the pen is blue";
     int n = strlen(str);
 
-    // Step 1: Reverse the entire string
-    int start = 0, end = n - 1;
-    while (start < end)
+    // Step 1: Reverse the whole string
+    int start = 0;
+    int end = n - 1;
+    while(start < end) 
     {
         char tmp = str[start];
         str[start] = str[end];
@@ -17,27 +18,47 @@ int main()
         end--;
     }
 
-    // Step 2: Reverse each word individually using your logic
-    start = 0;
-    for (int i = 0; i <= n; i++)
+    printf("Reversed string: %s\n", str);
+
+    // Step 2: Reverse each word individually
+    int starting = 0;
+    int i = 0;
+    
+    while(str[i]!='\0')
     {
-        if (str[i] == ' ' || str[i] == '\0')
+        if(str[i]==' ' || str[i]=='\0')
         {
-            int end = i - 1;
-
-            while (start < end)
+            int end;
+            
+            if(str[i+1]=='\0')
+            end = i;
+            
+            else
+            end = i -1;
+            
+            int s = starting;
+            int e = end;
+            
+            while(s<e)
             {
-                char tmp = str[start];
-                str[start] = str[end];
-                str[end] = tmp;
-                start++;
-                end--;
+                char tmp = str[s];
+                str[s] = str[e];
+                str[e] = tmp;
+                
+                s++;
+                e--;
             }
-
-            start = i + 1;
+            
+              starting = i+1;
+            
         }
+        
+        i++;
+        
+      
     }
 
-    printf("The new string is:\n%s", str);
+    printf("Reversed words string: %s\n", str);
+
     return 0;
 }
