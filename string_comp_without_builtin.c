0b9600f17d85611435a0052678aa9c794e8dc0a5
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-10-21 17:02:48 +0530

    Update string_comp_without_builtin.c

--- string_comp_without_builtin.c
+++ string_comp_without_builtin.c
@@ -3,49 +3,35 @@
 
 int main() 
 {
-    char str[] = "Hi sai";
-    int n = strlen(str);
-
-    // Step 1: Reverse the entire string
-    int start = 0, end = n - 1;
-    while (start < end)
+    char str1[]="sail";
+    char str2[]="sai";
+    
+    int i=0;
+    
+    int comp=0;
+    
+    while(str1[i]!='\0' && str2[i]!='\0')
     {
-        char tmp = str[start];
-        str[start] = str[end];
-        str[end] = tmp;
-        start++;
-        end--;
-    }
-
-    printf("Reversed string: %s\n", str);
-
-    // Step 2: Reverse each word individually
-    start = 0;  // start of the current word
-    int i = 0;  // iterator through the string
-
-    while (i <= n)  // include '\0' to handle the last word
-    {
-        if (str[i] == ' ' || str[i] == '\0')
+        if(str1[i]!=str2[i])
         {
-            end = i - 1;  // end of the current word
-
-            int s = start;
-            int e = end;
-            while (s < e)  // reverse characters of the current word
-            {
-                char tmp = str[s];
-                str[s] = str[e];
-                str[e] = tmp;
-                s++;
-                e--;
-            }
-
-            start = i + 1;  // move start to the next word
+            comp = str1[i] - str2[i];
+            break;
         }
         i++;
     }
-
-    printf("Reversed words string: %s\n", str);
+    
+    if(comp==0)
+    {
+        comp = str1[i]-str2[i];
+    }
+    
+    if(comp==0)
+    printf("Both string are equal");
+    else if(comp>0)
+    printf("string 1 is bigger than string2");
+    else
+    printf("string 1 is smaller than string 2");
 
     return 0;
+    
 }
