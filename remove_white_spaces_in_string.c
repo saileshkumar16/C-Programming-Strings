0dbb2b90654ba5bcc1b847500592642609e76a08
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-11-02 09:33:03 +0530

    Update remove_white_spaces_in_string.c

--- remove_white_spaces_in_string.c
+++ remove_white_spaces_in_string.c
@@ -1,23 +1,26 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 #include <string.h>
-
-int main()
+int main() 
 {
     char str[]="h e ll";
+    int n = strlen(str);
+    printf("String before removing spaces is |%s|\n",str);
     
-    int j=0;
-    for(int i=0; str[i]!='\0';i++)
+    for(int i=0; i<n; i++)
     {
-        if(str[i]!=' ')
+        if(str[i]==' ')
         {
-            str[j]=str[i];
-            j++;
+            for(int j=i; j<n; j++)
+            {
+                str[j] = str[j+1];
+            }
+            
+            n--;
+            i--;
         }
     }
-    
-    str[j]='\0';
-    
-    printf("%s",str);
-    
+    printf("String after removing spaces is |%s|",str);
+
     return 0;
 }
