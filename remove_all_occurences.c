c77012b996a19bffcb59ab8ab76a477ae50eb2b7
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-11-02 10:47:14 +0530

    Update remove_all_occurences.c

--- remove_all_occurences.c
+++ remove_all_occurences.c
@@ -1,60 +1,29 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 #include <string.h>
-
-void remove_char_j0(char str[], char ch) 
-{
-    int n = strlen(str);
-    for (int i = 0; i < n; i++) 
-    {
-        for (int j = 0; j < n; j++)  // Scans from start each time
-        {
-            if (str[j] == ch) 
-            {
-                // Shift left
-                for (int k = j; k < n - 1; k++) 
-                {
-                    str[k] = str[k + 1];
-                }
-                n--;
-                j--; // stay at same index after shift
-            }
-        }
-    }
-    str[n] = '\0';
-    printf("After removing '%c' (j=0): %s\n", ch, str);
-}
-
-void remove_char_ji(char str[], char ch) 
-{
-    int n = strlen(str);
-    for (int i = 0; i < n; i++) 
-    {
-        for (int j = i; j < n; j++)  // Scans only from current i
-        {
-            if (str[j] == ch) 
-            {
-                // Shift left
-                for (int k = j; k < n - 1; k++) 
-                {
-                    str[k] = str[k + 1];
-                }
-                n--;
-                j--; // stay at same index after shift
-            }
-        }
-    }
-    str[n] = '\0';
-    printf("After removing '%c' (j=i): %s\n", ch, str);
-}
-
 int main() 
 {
-    char str1[] = "saai";
-    char str2[] = "saai";
-    char ch = 'a';
-
-    remove_char_j0(str1, ch);
-    remove_char_ji(str2, ch);
+    char str[]="baloon";
+    
+    char ch = 'o';
+    
+    int n = strlen(str);
+    
+    
+    for(int i=0; i<n; i++)
+    {
+        if(str[i]==ch)
+        {
+            for(int j=i; j<n; j++)
+            {
+                str[j] = str[j+1];
+            }
+            n--;
+            i--;
+        }
+    }
+    
+    printf("%s",str);
 
     return 0;
 }
